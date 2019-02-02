18cb6d4dfffffcf9cdec3e8dd09f4ff999ce601f
coaxial <hi@64b.it>
2019-02-02 15:37:01 -0500

    Change behavior, add npm run publish

--- tasks/latex.js
+++ tasks/latex.js
@@ -1,13 +1,9 @@
 const shell = require("gulp-shell");
-const gutil = require("gulp-util");
 
 module.exports = (gulp) => {
   gulp.task('latex2pdf', shell.task([
     'bash lib/pdf_all.sh'
   ]));
 
-  gulp.task('compile', gulp.series('latex2pdf', (done) => {
-    gutil.log('🖨  Change detected, recompiling!');
-    done();
-  }));
+  gulp.task('compile', gulp.series('latex2pdf'));
 };
