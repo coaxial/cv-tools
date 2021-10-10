307dce05e5d0646e65620f415f34637733e7cc08
coaxial <c@64b.it>
2021-10-10 09:47:21 +0200

    Fix tasks

--- tasks/latex.js
+++ tasks/latex.js
@@ -2,13 +2,11 @@ const shell = require("gulp-shell");
 const gutil = require("gulp-util");
 
 module.exports = (gulp) => {
-  gulp.task('compile', (done) => {
-    gutil.log('🖨  Change detected, recompiling!');
-    gulp.series('latex2pdf');
-    done();
-  });
-
   gulp.task('latex2pdf', shell.task([
-    'bash lib/pdf_all.sh'
+    'bash lib/pdf_all.sh > /dev/null',
   ]));
+
+  gulp.task('compile', gulp.series('latex2pdf'), () => {
+    gutil.log('🖨  Change detected, recompiling!');
+  });
 };
